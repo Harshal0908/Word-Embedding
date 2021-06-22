42055563bd292903ad415aae10befef557c6819d
Harshal0908 <harshal.humane1999@gmail.com>
2021-06-22 21:28:00 +0530

    Created Git Ignore and intialised sentences

---
+++ main.py
@@ -0,0 +1,11 @@
+from tensorflow.keras.preprocessing.text import one_hot
+
+sent = ['The glass of milk',
+        'the glass of juice',
+        'the cup of Tea',
+        'I am a good boy',
+        'I am a good developer',
+        'Understan the meaning of words',
+        'your videos are good',]
+
+print(sent)
