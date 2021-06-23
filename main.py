742e09e90ca5eba1a926fe7d8503670d30dff2c6
Harshal0908 <harshal.humane1999@gmail.com>
2021-06-23 09:50:27 +0530

    CREATED EMBEDDED DOCS

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from tensorflow.keras.preprocessing.text import one_hot
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+import numpy as np
 
 sent = ['The glass of milk',
         'the glass of juice',
@@ -14,3 +18,7 @@ voc_size = 10000
 
 onehot_repr = [one_hot(words,voc_size) for words in sent]
 print(onehot_repr)
+
+sent_len = 8
+embedded_docs = pad_sequences(onehot_repr,padding = 'pre',maxlen=sent_len)
+print(embedded_docs)
